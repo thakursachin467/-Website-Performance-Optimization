02ff89be8cc8e5a2cf39315a5530913db3fe8841
Sachin Thakur <thakursachin467@gmail.com>
2017-08-24 23:08:01 +0530

    optimized

--- views/js/main.js
+++ views/js/main.js
@@ -511,7 +511,7 @@ function updatePositions() {
   // Super easy to create custom metrics.
   window.performance.mark("mark_end_frame");
   window.performance.measure("measure_frame_duration", "mark_start_frame", "mark_end_frame");
-  if (frame % 60 === 0) {
+  if (frame % 10 === 0) {
     var timesToUpdatePosition = window.performance.getEntriesByName("measure_frame_duration");
     logAverageFrame(timesToUpdatePosition);
   }
